531312a61ca5086c4e21e65b9042fffdccc409a3
Areeb Khan <areebk@umich.edu>
2023-11-08 16:51:54 -0500

    added images

--- skillsync-app/src/app/page.tsx
+++ skillsync-app/src/app/page.tsx
@@ -3,6 +3,7 @@
 import Image from 'next/image'
 import ContinueButton from '@/components/ContinueButton';
 import SkillSyncLogo from '@/components/skillsyncLogo';
+import FeatureText from '@/components/featureText';
 import Link from 'next/link'
 // Import Firebase Analytics to initialize it (if you're going to use it)
 import { analytics } from '@/lib/firebase'; // Adjust the path to where your firebase.js file is located
@@ -14,9 +15,13 @@ export default function Home() {
     <div>
       <SkillSyncLogo size='large' />
 
+
       <Link href="/2-role_input" passHref>
           <ContinueButton number="1" />
       </Link>
+
+      <FeatureText/>
+        
     </div>
-  ); 
+  );
 }
