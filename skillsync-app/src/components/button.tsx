92feab2222a0604a9b6aa15e8e41b95edc53eade
emmarigg <emmarigg@umich.edu>
2023-11-08 12:43:43 -0500

    button added

---
+++ skillsync-app/src/components/button.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface ButtonProps {
+    text: string;
+    onClick: () => void;
+}
+
+const Button = ({ text, onClick }: ButtonProps) => {
+    return (
+        <button onClick={onClick}>
+            {text}
+        </button>
+    );
+};
+
+export default Button;
